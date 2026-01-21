621e5da707f8aee3bae8bc19f55531c74c6982eb
rjcass13 <rjcass13@gmail.com>
2026-01-20 20:04:55 -0700

    unit1

---
+++ StarPower/data.R
@@ -0,0 +1,11 @@
+# NBA All-star game
+
+# Questions to investigate:
+# When is the game played? 3rd Sunday of February (midpoint of NBA season)
+# How are players chosen? Currently, 50% fans vote, 25% each for media and player
+# - Starting 2026, voting not limited to position
+# - 12 players from each conference picked (East vs. West)
+
+# Other interesting things:
+# The games tend to be more 'showy', less defense, more impressive shots, etc.
+# Players do get paid, and money also goes to charity
